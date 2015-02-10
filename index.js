bf4b4d0f61a1dd8c79e60db75bc98b9dcfa4d3ff
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-02-09 22:45:19 -0500

    added todo

--- index.js
+++ index.js
@@ -9,3 +9,8 @@ ggit.commits.all(repoFolder)
     console.log(list.length + ' commit(s)');
   })
   .done();
+
+// look at the git blame for files at head
+// then look at each commit
+// and see how many files were modified in the commit K
+// compute survival rate for commit K
