0d574b05aecbb7522426a3c68334795080a8fca6
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-03-07 05:39:41 -0700

    added a few quality modules

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
+require('cute-stack')(require('bad-line'));
+
+require('lazy-ass');
+var check = require('check-more-types');
+
 var ggit = require('ggit');
 require('console.table');
 
 var repoFolder = __dirname;
 
+function printCommitsList(list) {
+  la(check.array(list), list);
+  console.table(list);
+  console.log(list.length + ' commit(s)');
+}
+
 ggit.commits.all(repoFolder)
-  .then(function (list) {
-    console.table(list);
-    console.log(list.length + ' commit(s)');
-  })
+  .then(printCommitsList)
   .done();
 
 // look at the git blame for files at head
